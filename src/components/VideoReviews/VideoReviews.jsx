b3cf93c372efd3463a77c67a3d3b1f6209aebb99
eugenekh81 <eugene.khablenko@gmail.com>
2023-10-07 21:48:03 +0300

    fixed video review size

---
+++ src/components/VideoReviews/VideoReviews.jsx
@@ -0,0 +1,19 @@
+import './VideoReviews.scss';
+
+export const VideoReviews = () => {
+  return (
+    <section className="video-reviews">
+      <h2 className="video-reviews__title">
+        Video Reviews
+      </h2>
+<div className="container">
+      <div className="video-reviews__item">
+        <iframe src="https://www.youtube.com/embed/fEuExMpBU_o?si=x6k3GVOaYExfrJSb" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      </div>
+      <div className="video-reviews__item">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Go9A6_EpRn8?si=Lux-SfoVLVAoq_Np" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+</div>
+    </section>
+  );
+};
