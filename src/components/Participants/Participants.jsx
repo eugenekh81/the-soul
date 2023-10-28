6112415c8b5add52b3c70b96ae9d17159593fd06
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-10-28 10:12:47 +0300

    added styles to participants section

--- src/components/Participants/Participants.jsx
+++ src/components/Participants/Participants.jsx
@@ -10,42 +10,41 @@ export const Participants = () => (
     <div className='container container--geo'>
       <h2 className='section__title'>Participants</h2>
 
-    <div className='participants__content'>
-      <p className='section__description participants__description'>
-        Participants filled a registration form, which allowed us to get to know
-        each person better. The war forced many people to leave their homes, so
-        participants joined our programme from all over the world and Ukraine.
-        In terms of the proportion, 29% are outside Ukraine and are distributed
-        across Europe and the world, and 71% are in Ukraine
-      </p>
-      <div className="participants__image participants__image--ukraine-map">
-            <img
-              src={ukraineMap}
-              alt='Ukraine demographics'
-            />
-      </div>
-      <div className="participants__image participants__image--europe-map">
-      <img
-        src={europeMap}
-        alt='Europe demographics'
-      />
-      </div>
-
-
+      <div className='participants__content'>
+        <p className='section__description participants__description'>
+          Participants filled a registration form, which allowed us to get to
+          know each person better. The war forced many people to leave their
+          homes, so participants joined our programme from all over the world
+          and Ukraine. In terms of the proportion, 29% are outside Ukraine and
+          are distributed across Europe and the world, and 71% are in Ukraine
+        </p>
+        <div className='participants__image participants__image--ukraine-map'>
+          <img src={ukraineMap} alt='Ukraine demographics' />
+        </div>
+        <div className='participants__image participants__image--europe-map'>
+          <img src={europeMap} alt='Europe demographics' />
+        </div>
       </div>
 
       <h3 className='section__subtitle'>AGE STRUCTURE</h3>
 
       <Row className='participants__age-structure'>
-        <Col xs={12} sm={4} lg={4}>
+        <Col xs={12} sm={4} lg={4} className='participants__list-container'>
           <ul className='participants__list'>
             <li className='participants__list-item'>
-              47% - women aged 36 - 50 y.o.
+              <div className='participants__percentage'>47%</div>
+              <div className='participant__category'>
+                women aged 36 - 50 y.o.
+              </div>
             </li>
             <li className='participants__list-item'>
-              20% - women under 35 y.o.
+              <div className='participants__percentage'>20%</div>
+              <div className='participants__category'>women under 35 y.o.</div>
             </li>
-            <li className='participants__list-item'>13% - women aged 50+</li>
+            <li className='participants__list-item'>
+              <div className='participants__percentage'>13%</div>
+              <div className='participants__category'>women aged 50+</div>
+            </li>
           </ul>
         </Col>
         <Col xs={10} sm={8} lg={4}>
