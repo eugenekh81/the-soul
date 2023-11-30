be09f03d4397a7b82c6c0eb3ddd1f197a3488f01
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-11-30 04:18:25 +0200

    fixed donate now button

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -2,15 +2,19 @@ import sprite from '/images/concepts/concepts.svg';
 import './Donate.scss';
 import { useContext } from 'react';
 import { LangContext } from '../../context/LangContextProvider';
+import { NavLink } from 'react-router-dom';
 
 export const Donate = () => {
   const { lang } = useContext(LangContext);
   return (
-    <a href='#' className='button section__button donate__button'>
+    <NavLink
+      to={`/${lang}/donate`}
+      className='button section__button donate__button'
+    >
       <svg className='donate__icon'>
         <use href={sprite + '#plant'} />
       </svg>
       {lang === 'en' ? 'Donate now' : 'Допомогти'}
-    </a>
+    </NavLink>
   );
 };
